5c0e052ed2aa96e5972e820e1c9d8ca7b6c0cadc
umairayub79 <umairayub79@gmail.com>
2022-01-19 20:07:53 +0500

    fixed a bug

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import axios from 'axios'
 import Movie from './Movie'
@@ -15,9 +15,9 @@ const Movies = () => {
     const getMovies = async () => {
         let response = []
         try {
-            if(query !== undefined && query !== null && query !== ""){
+            if (query !== undefined && query !== null && query !== "") {
                 response = await axios.get(`https://yts.mx/api/v2/list_movies.json?page=${page}&query_term=${query}`)
-            }else {
+            } else {
                 response = await axios.get(`https://yts.mx/api/v2/list_movies.json?page=${page}`)
             }
             setResponse(response.data.data)
@@ -34,18 +34,24 @@ const Movies = () => {
     }
 
     useEffect(() => {
+        setPage(1)
+    }, [query])
+    
+    useEffect(() => {
         setLoading(true)
         getMovies()
-    }, [page,query])
+    }, [page, query])
+
+
 
     return (
         <div>
             {!loading ? (query ? <div>Search Results: </div> : "") : <div></div>}
             <div className='p-5 grid grid-cols-2 gap-4 md:grid-cols-4'>
-                {!loading ? (movies ? movies.map((movie) => (<Movie key={movie.id} movie={movie}/>)) : <h1>Nothing found</h1>) : <h1>Loading</h1>}            
+                {!loading ? (movies ? movies.map((movie) => (<Movie key={movie.id} movie={movie} />)) : <h1>Nothing found</h1>) : <h1>Loading</h1>}
             </div>
             <div>
-                {!loading ? <Pagination total_items={response.movie_count} currentPage={page} gotoPage={gotoPage}/>: "" }
+                {!loading ? <Pagination total_items={response.movie_count} currentPage={page} gotoPage={gotoPage} /> : ""}
             </div>
         </div>
     )
