59d302695ea045109e198f82e7b8340559e70619
umairayub79 <umairayub79@gmail.com>
2022-03-03 11:26:47 +0500

    fixed navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import SearchBar from './Searchbar'
 const Navbar = () => {
     return (
-        <div className='border-b text-black p-2 flex flex-col items-center justify-center md:justify-between md:flex-row'>
+        <div className='sticky top-0 z-50 bg-white border-b text-black p-2 flex flex-col items-center justify-center md:justify-between md:flex-row'>
             <h1 className='p-2 text-2xl md:text-4xl font-extrabold'>Mflix</h1>
             <SearchBar />
         </div>
